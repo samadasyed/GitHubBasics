64a8caa0323dc3e6c2ec6958a7d6815d3124e6ba
samadasyed <samadasyed@hotmail.com>
2025-11-04 16:01:28 -0500

    Adding comment file in cpp

---
+++ GithubBasics.cpp
@@ -0,0 +1,20 @@
+/**
+    Create a new repo on the command line 
+ 
+    echo "# (NameofRepo)" >> README.md
+    git init
+    git add README.md
+    git commit -m "first commit"
+    git branch -M main
+    git remote add origin (http URL)
+    git push -u origin main
+ 
+    Push an existing repo from the command line 
+    git remote add origin (http URL)
+    git branch -M main
+    git push -u origin main
+
+
+
+ */
+
